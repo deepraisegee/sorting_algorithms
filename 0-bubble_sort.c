bb525d3a3f5508c8c769faaa8b6412141ed2e284
MyssyB <beulahagiande@gmail.com>
2024-05-01 05:19:27 -0700

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++){
+		if i < j
+}
+int main(void)
+{
+}
+
