b7aa820804af572fe4fe3df89386d0b949bde4ae
MyssyB <beulahagiande@gmail.com>
2024-05-02 05:08:12 -0700

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,10 +3,14 @@
 #include "sort.h"
 
 /**
-  * bubble_sort - sorts an array of integers in ascending
-  * order using the Bubble sort algorithm
-  * @array: pointer to the array
-  * @size: size of the array
+  * bubble_sort - sorts an array of integers in ascending order using the Bubble sort algorithm
+  *
+  * swap - switchs 2 integers in an array
+  *
+  * @array pointer to the arrayt
+  * @size size of the array
+  * @*a pointer to the first integer to swap
+  * @*b pointer to the second integer
   *
   * Return: void
   */
@@ -14,6 +18,7 @@
 void swap(int *a, int *b)
 {
 	int tmp;
+	
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
@@ -27,14 +32,7 @@ void bubble_sort(int *array, size_t size)
 		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
-		/**
-		 * im thinking, instead of swap, why not:
-		 * create a temp variable and assign it the
-		 * value of i: temp = array[i];
-		 * array[i] = array[i + 1]
-		 * array [i + 1] = temp
-		 */
-		swap(&array[j], &array[j + 1]);
+				swap(&array[j], &array[j + 1]);
 		print_array(array, size);
 		/**
 		 * tmp = array[j];
