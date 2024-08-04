ec0a740868880c6ae7b850253c07eade9355e70b
MyssyB <beulahagiande@gmail.com>
2024-08-04 09:10:18 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,30 +15,25 @@
   * Return: void
   */
 
-void swap(int *a, int *b)
-{
-	int tmp;
-	
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	unsigned int i, j;
+	int tmp;
 
-	for (i = 0; i < size - 1; i++)
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - i -1; j++)
 		{
 			if (array[j] > array[j + 1])
-				swap(&array[j], &array[j + 1]);
-		print_array(array, size);
-		/**
-		 * tmp = array[j];
-		array[j] = array[j + 1];
-		array[j + 1] = tmp;
-		*/
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
 		}
 	}
 }
